3a6da0d9e1c75260e10e49b1d8dd4d7f3df428de
Sachin Mahajan <sachinm78@gmail.com>
2019-06-12 15:10:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function writeCards( namesArray, event ) {
 }
 
 function countdown( countdown ) {
+  let countown = 10
   while ( countdown > 0 ) {
     console.log( countdown );
     countdown -= 1;
